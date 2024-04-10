b5e69ad0c3297e44e109283a68db1db44d862da0
Chloedj <155569174+Chloedj@users.noreply.github.com>
2024-04-10 17:57:36 +0100

    Started Hero Styling

--- recipe-app/src/containers/hero/hero.js
+++ recipe-app/src/containers/hero/hero.js
@@ -6,16 +6,16 @@ const Hero = () => {
   return (
     <div>
       <div className='recipe__hero section__padding' id="#home">
-        <div className="recipe__header-content">
-      <h1 className="recipe__header-content__h1">Cooking Made Fun and Easy: Unleash Your Inner Chef</h1>
-      <p>Discover more than <span className='recipe__header-content__span'>10,000 recipes</span> in your hand with the best recipe. Help you to find the easiest way to cook</p>
+        <div className="recipe__hero-content">
+      <h1>Cooking Made Fun and Easy: Unleash Your Inner Chef</h1>
+      <p>Discover more than <span className='accent'>10,000 recipes</span> in your hand with the best recipe. Help you to find the easiest way to cook</p>
 
-      <div className="recipe__header-content__input">
+      <div className="recipe__hero-content__input">
         <button type="button">Explore Recipes</button>
       </div>
     </div>
 
-    <div className="recipe__header-image">
+    <div className="recipe__hero-image">
       <img src={plate} alt='plate'/>
     </div>
       </div>
