bbb82c37567f1466107a93e9b4ed03a965153300
Chloedj <155569174+Chloedj@users.noreply.github.com>
2024-04-10 18:16:21 +0100

    Fixed Button styling to stop navbar moving

--- recipe-app/src/containers/hero/hero.js
+++ recipe-app/src/containers/hero/hero.js
@@ -11,7 +11,7 @@ const Hero = () => {
       <p>Discover more than <span className='accent'>10,000 recipes</span> in your hand with the best recipe. Help you to find the easiest way to cook</p>
 
       <div className="recipe__hero-content__input">
-        <button type="button">Explore Recipes</button>
+        <button type="button" class="button">Explore Recipes</button>
       </div>
     </div>
 
