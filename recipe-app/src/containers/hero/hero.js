975823b249d379a5b1123e58249d1d52dd7c41a5
Chloedj <155569174+Chloedj@users.noreply.github.com>
2024-04-11 15:52:38 +0100

    Finished Styling Hero, added veg

--- recipe-app/src/containers/hero/hero.js
+++ recipe-app/src/containers/hero/hero.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import './hero.css'
 import plate from '../../assets/plate.png'
+import Garlic from '../../assets/Garlic.png'
+import Tomato from '../../assets/Tomato.png'
 
 const Hero = () => {
   return (
     <div>
       <div className='recipe__hero section__padding' id="#home">
         <div className="recipe__hero-content">
-      <h1>Cooking Made Fun and Easy: Unleash Your Inner Chef</h1>
-      <p>Discover more than <span className='accent'>10,000 recipes</span> in your hand with the best recipe. Help you to find the easiest way to cook</p>
-
+           <img src={Tomato} alt="Tomato" className="Tomato" />
+      <h1>
+        
+  <span className="h1-block">Cooking Made Fun</span>
+  <span className="h1-block"> and Easy: Unleash </span>
+  <span className="h1-block">Your Inner Chef</span>
+</h1>
+      <p>Discover more than <span className='accent'>10,000 recipes</span> in your hand with the<br /> best recipe. Help you to find the easiest way to cook</p>
+      <img src={Garlic} alt="Garlic" className="Garlic" />
       <div className="recipe__hero-content__input">
         <button type="button" class="button">Explore Recipes</button>
       </div>
@@ -19,7 +27,8 @@ const Hero = () => {
       <img src={plate} alt='plate'/>
     </div>
       </div>
-    </div>
+         </div>
+    
   )
 }
 
