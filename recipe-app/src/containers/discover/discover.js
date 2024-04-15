06d9aaa60423083ccd8feda8c7c59a48c0aff810
Chloedj <155569174+Chloedj@users.noreply.github.com>
2024-04-15 16:01:54 +0100

    Styled into to discovery section

--- recipe-app/src/containers/discover/discover.js
+++ recipe-app/src/containers/discover/discover.js
@@ -3,8 +3,14 @@ import './discover.css'
 
 const Discover = () => {
   return (
-    <div>
-      Discover
+    <div className='recipe__discover-title section__padding'>
+      <div className='recipe__discover-title-content'>
+      <h2>Discover, Create, Share</h2>
+      <p>Check our most popular recipes of this week</p>
+      </div>
+      <div className='recipe__discover-title-button'>
+        <button type='button'>See All</button>
+      </div>
     </div>
   )
 }
