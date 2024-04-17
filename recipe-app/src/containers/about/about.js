a04f3100393fbbb936d3968a79b5ae71a7a2d7c6
Chloedj <155569174+Chloedj@users.noreply.github.com>
2024-04-17 18:04:53 +0100

    Added content box to about section

--- recipe-app/src/containers/about/about.js
+++ recipe-app/src/containers/about/about.js
@@ -6,12 +6,12 @@ const About = () => {
   return (
     <div className='about__container' style={{ backgroundImage: `url(${backgroundImage})` }}>
       <div className='about__container-content'>
-        <div>
+        <div className='about__container-details'>
           <h3>About Us</h3>
           <p>Our recipes are the heart and soul of our culinary community, and they reflect our commitment to providing you with memorable and delightful dining experiences.</p>
-          
+          <div className='about__container-details-button'>
         <button type='button'><a href="#home">Learn More</a></button>
-      
+      </div>
           </div>
       </div>
     </div>
