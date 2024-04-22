bcf1d00e2a009c993e396197560a694cf0c70ca0
Chloedj <155569174+Chloedj@users.noreply.github.com>
2024-04-22 17:24:41 +0100

    Started to style community recipe card

--- recipe-app/src/components/communityrecipe/communityrecipe.js
+++ recipe-app/src/components/communityrecipe/communityrecipe.js
@@ -1,10 +1,30 @@
 import React from 'react'
 import './communityrecipe.css'
+import stars from '../../assets/5stars.png'
+import like from '../../assets/like.png'
+import share from '../../assets/share.png'
 
-const CommunityRecipe = ({author, authorimage, description, recipeImageUrl}) => {
+const CommunityRecipe = ({author, authorimage, name, description, recipeImageUrl, likes}) => {
   return (
-    <div>
-      CommunityRecipe
+    <div className='community__recipe-container'>
+      <div className='community__recipe-details'>
+        <img src={authorimage} alt='author' />
+        <div>
+        <h3>{name}</h3>
+        <p>{author}</p>
+        </div>
+      </div>
+      <div className='community__recipe-description'>
+        <img src={stars} alt='5 stars' />
+        <p>{description}</p>
+      </div>
+      <div className='community__recipe-image'>
+      <img src={recipeImageUrl} alt='recipe' />
+      </div>
+      <img src={like} alt='like' />
+      <h5>{likes}</h5>
+      <img src={share} alt='share' />
+      <h5>Share</h5>
     </div>
   )
 }
--- recipe-app/src/containers/community/recipedata.js
+++ recipe-app/src/containers/community/recipedata.js
@@ -4,11 +4,12 @@ import spagbol from "../../assets/spagbol.png";
 const recipeData = [
   {
     id: 1,
+    authorimage: LadyRudy,
     name: "Spaghetti Bolognese",
     author: "Lady Rudy",
-    authorimage: LadyRudy,
     description: "I have to say, your Spaghetti Bolognese recipe is nothing short of amazing! I've always been a fan of Italian cuisine, but I was a bit intimidated by the idea of making this classic at home.",
     recipeImageUrl: spagbol,
+    likes: 2,
   },
 ]
 
