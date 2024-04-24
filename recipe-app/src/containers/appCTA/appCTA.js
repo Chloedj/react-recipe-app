3074ed6dea0c8b99d57de9a68394a03e431ee076
Chloedj <155569174+Chloedj@users.noreply.github.com>
2024-04-24 15:42:28 +0100

    Added app section

--- recipe-app/src/containers/appCTA/appCTA.js
+++ recipe-app/src/containers/appCTA/appCTA.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import './appCTA.css'
+import appstore from '../../assets/appstore.png'
+import googleplay from '../../assets/googleplay.png'
+import iphone from '../../assets/iphone.png'
 
 const AppCTA = () => {
   return (
-    <div>
-      AppCTA
+    <div className='appCTA__container section__padding'>
+      <div className='appCTA__content'>
+        <h3>Embrace the joy of cooking with get it on your iPhone or Android Your kitchen adventure begins now!</h3>
+        <div className='appCTA__content-appdownload'>
+          <img src={appstore} alt='appstore' />
+          <img src={googleplay} alt='googleplay' />
+        </div>
+      </div>
+      <div className='appCTA__image'>
+          <img src={iphone} alt='iphone' />
+      </div>
     </div>
   )
 }
